f0234adc3cf43b352d739d2eafb7aeb6059cab35
VCCICCV <chaochen497@gmail.com>
2023-06-23 21:13:12 +0800

    考试停更

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,13 @@
 <script setup>
 import HomeBanner from '@/views/Home/components/HomeBanner.vue'
+import Example from '@/views/Home/components/Example.vue'
 </script>
 
 <template>
     <!-- 轮播图 -->
     <HomeBanner></HomeBanner>
+    <!-- 测试 -->
+    <Example></Example>
     <!-- 精选 -->
     <!-- 热门 -->
     <!-- 受欢迎 -->
